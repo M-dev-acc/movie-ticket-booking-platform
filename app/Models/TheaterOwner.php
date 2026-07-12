a38370edfdea6895ffca62fb4bb6d4859fd62b16
M-dev-acc <75662164+M-dev-acc@users.noreply.github.com>
2026-07-13 02:18:58 +0530

    Update module: fix typos && add relationships

--- app/Models/TheaterOwner.php
+++ app/Models/TheaterOwner.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class TheaterOwner extends Model
@@ -10,13 +11,20 @@ class TheaterOwner extends Model
     protected array $fillable = [
         'theater_id',
         'user_id',
-        'assgined_by',
+        'assigned_by',
     ];
 
     public function theater() : BelongsToMany {
-        return $this->belongsToMany(Theater::class, 'theater_owners', 'theaeter_id')
+        return $this->belongsToMany(Theater::class, 'theater_owners', 'theater_id')
         ->withPivot('assigned_by')
         ->withTimestamps();
     }
 
+    public function owner() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
+
+    public function assignedBy() : BelongsTo {
+        return $this->belongsTo(User::class);
+    }
 }
