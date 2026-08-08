e63bce3a909a91d6d19eb810e65ba77331679ecd
M-dev-acc <75662164+M-dev-acc@users.noreply.github.com>
2026-08-08 18:04:11 +0530

    Update module: add payment controller

---
+++ app/Http/Controllers/PaymentController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Booking;
+use App\Services\Payment\Contracts\PaymentGatewayInterface;
+use App\Services\Payment\PaymentService;
+use App\Traits\ApiResponse;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class PaymentController extends Controller
+{
+    use ApiResponse;
+
+    public function __construct(
+        private PaymentService $service,
+        private PaymentGatewayInterface $gateway
+    ) {
+    }
+
+    public function intiate(Request $request): JsonResponse
+    {
+        $request->validate([
+            'booking_code' => 'required|string|exists:bookings,code',
+        ]);
+
+        $booking = Booking::where('code', $request->booking_code)
+            ->where('user_id', auth()->id())
+            ->firstOrFail();
+        if($booking->status !== Booking::STATUS_RESERVED) {
+            return $this->error(
+                'This booking is not in payable state. Status: ' . $booking->status,
+                422
+            );
+        }
+
+        $orderData = $this->service->initiate($booking);
+
+        return $this->success(
+            data: $orderData,
+            message: 'Payment order created successfullly.');
+    }
+
+    public function verify(Request $request): JsonResponse
+    {
+        $request->validate([
+            'razorpay_order_id' => 'required|string',
+            'razorpay_payment_id' => 'required|string',
+            'razorpay_signature' => 'required|string',
+        ]);
+
+        $result = $this->gateway->verifyPaymentSignature(
+            $request->razorpay_order_id,
+            $request->razorpay_payment_id,
+            $request->razorpay_signature
+        );
+        if (!$result['status']) {
+            return $this->error(
+                statusCode: 422,
+                message:'Payment verification failed'
+            );
+        }
+
+        return $this->success(
+            data: null,
+            message: 'Payment verification successful.',
+        );
+    }
+}
